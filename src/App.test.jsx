e16287d1a6a11811f98ed87154fd16d64312d0ae
Manav Misra <manavm@visionwebsoft.com>
2022-10-09 18:01:31 -0500

    test: :white_check_mark: deletes a todo

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
+
+function setup(component) {
+  return {
+    user: userEvent.setup(),
+    ...render(component),
+  };
+}
 
 describe("App", () => {
   it("loads todos", async () => {
@@ -7,5 +15,16 @@ describe("App", () => {
 
     expect(screen.getByText("Loading...")).toBeInTheDocument();
     expect(await screen.findByText("delectus aut autem")).toBeInTheDocument();
+  });
+
+  it("deletes a todo", async () => {
+    const { user } = setup(<App />);
+
+    const todo = await screen.findByText("delectus aut autem");
+    const deleteButton = within(todo).getByRole("button");
+
+    await user.click(deleteButton);
+
+    expect(todo).not.toBeInTheDocument();
   });
 });
