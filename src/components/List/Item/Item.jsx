45a6d0e4512657882dc17a69d77d1bdc71498029
Manav Misra <manavm@visionwebsoft.com>
2022-10-09 17:06:02 -0500

    Avoid children by using a 'renderContent' fxn.

--- src/components/List/Item/Item.jsx
+++ src/components/List/Item/Item.jsx
@@ -1,11 +1,21 @@
 import PropTypes from "prop-types";
 
-function Item({ txt }) {
-  return <li>{txt}</li>;
+function Item({ txt, renderContent }) {
+  return (
+    <li>
+      {txt}
+      {renderContent && renderContent()}
+    </li>
+  );
 }
 
 Item.propTypes = {
   txt: PropTypes.string.isRequired,
+  renderContent: PropTypes.func,
+};
+
+Item.defaultProps = {
+  renderContent: null,
 };
 
 export default Item;
--- src/components/List/Item/Item.test.jsx
+++ src/components/List/Item/Item.test.jsx
@@ -1,12 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import Item from "./Item";
 
 describe("Item", () => {
   it("renders the item with the proper text", () => {
     render(<Item txt="Hello" />);
 
-    const item = screen.getByText("Hello");
+    const item = screen.getByRole("listitem");
 
     expect(item).toBeInTheDocument();
+  });
+
+  it("renders the item with the proper text and children", () => {
+    render(<Item txt="Hello" renderContent={() => <span>World</span>} />);
+
+    const item = screen.getByRole("listitem");
+    const content = within(item).getByText("World");
+
+    expect(item).toBeInTheDocument();
+    expect(content).toBeInTheDocument();
   });
 });
