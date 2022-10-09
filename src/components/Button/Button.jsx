ad8f0a51c40961679072772cb3e979f15f638318
Manav Misra <manavm@visionwebsoft.com>
2022-10-09 16:08:58 -0500

    Allow button to receive prop for type

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 
-function Button({ txt, handleClick }) {
+function Button({ txt, type, handleClick }) {
   return (
     <button
       onClick={handleClick}
       className="rounded bg-blue-500 px-4 py-2 text-white"
-      type="button"
+      type={type}
     >
       {txt}
     </button>
@@ -14,7 +14,12 @@ function Button({ txt, handleClick }) {
 
 Button.propTypes = {
   txt: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["button", "submit", "reset"]),
   handleClick: PropTypes.func.isRequired,
+};
+
+Button.defaultProps = {
+  type: "button",
 };
 
 export default Button;
